49e61c8aa4b738ffc00f1b591ccc8e3ce1f82a30
PixelsJr <oliver.aavik@gmail.com>
2025-03-15 10:52:13 +0200

    k

--- keemia.py
+++ keemia.py
@@ -1,4 +1,13 @@
-<<<<<<< HEAD
+
+# Format input: CO2 + H2O -> C6H12O6 + O2 
+# Format output: 6CO2 + H2O -> C6H12O6 + 6O2
+left_side = right_side = input().strip().split("->")
+left_side = left_side.split("+")
+right_side = right_side.split("+")
+
+for element in left_side:
+    element = element.strip()
+    
 from sympy import symbols, Eq, solve
 import re
 
@@ -42,14 +51,4 @@ def balance_equation(reactants, products):
 
     return " + ".join(f"{balanced_coeffs[i]} {reactants[i]}" for i in range(len(reactants))) + " -> " + \
            " + ".join(f"{balanced_coeffs[i+len(reactants)]} {products[i]}" for i in range(len(products)))
-=======
-# Format input: CO2 + H2O -> C6H12O6 + O2 
-# Format output: 6CO2 + H2O -> C6H12O6 + 6O2
-left_side = right_side = input().strip().split("->")
-left_side = left_side.split("+")
-right_side = right_side.split("+")
 
-for element in left_side:
-    element = element.strip()
-    
->>>>>>> fabi
