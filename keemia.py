210f9c8676fd76dd160d76e3f9befebd6681d6b8
lfkood <152791020+lfkood@users.noreply.github.com>
2025-03-15 10:37:42 +0200

    fixed input

--- keemia.py
+++ keemia.py
@@ -6,56 +6,36 @@ left_side, right_side = input().strip().split("->")
 left_side = left_side.split("+")
 right_side = right_side.split("+")
 
-counter_left = dd(int)
-counter_right = dd(int)
+def element_counter(reaction):
+    counter = dd(int)
 
-buf = ""
-buf_n = ""
-for element in left_side:
-    element: str = element.strip()
-    for char in element:
-        if char.isupper():
-            if buf:
-                if buf_n:
-                    counter_left[buf] += int(buf_n)
-                else:
-                    counter_left[buf] += 1
-            buf = char
-            buf_n = ""
-        elif char.islower():
-            buf += char
-        elif char.isdigit():
-            buf_n += char
-    if buf:
-        if buf_n:
-            counter_left[buf] += int(buf_n)
-        else:
-            counter_left[buf] += 1
+    buf = ""
+    buf_n = ""
+    for element in reaction:
+        element: str = element.strip()
+        for char in element:
+            if char.isupper():
+                if buf:
+                    if buf_n:
+                        counter[buf] += int(buf_n)
+                    else:
+                        counter[buf] += 1
+                buf = char
+                buf_n = ""
+            elif char.islower():
+                buf += char
+            elif char.isdigit():
+                buf_n += char
+        if buf:
+            if buf_n:
+                counter[buf] += int(buf_n)
+            else:
+                counter[buf] += 1
+    return counter
+
+counter_left = element_counter(left_side)
+counter_right = element_counter(right_side)
 
 
 print(counter_left)
-
-buf = ""
-buf_n = ""
-for element in right_side:
-    element: str = element.strip()
-    for char in element:
-        if char.isupper():
-            if buf:
-                if buf_n:
-                    counter_left[buf] += int(buf_n)
-                else:
-                    counter_left[buf] += 1
-            buf = char
-            buf_n = ""
-        elif char.islower():
-            buf += char
-        elif char.isdigit():
-            buf_n += char
-    if buf:
-        if buf_n:
-            counter_left[buf] += int(buf_n)
-        else:
-            counter_left[buf] += 1
-
 print(counter_right)
