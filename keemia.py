35e670371ad1a9ee74947f76957240b1ffc613ea
lfkood <152791020+lfkood@users.noreply.github.com>
2025-03-15 10:18:25 +0200

    a

--- keemia.py
+++ keemia.py
@@ -1,1 +1,12 @@
-stdin = input().strip().split()
+# Format input: CO2 + H2O -> C6H12O6 + O2 
+# Format output: 6CO2 + H2O -> C6H12O6 + 6O2
+left_side = right_side = input().strip().split("->")
+left_side = left_side.split("+")
+right_side = right_side.split("+")
+
+for element in left_side:
+    element = element.strip()
+    if element[0].isdigit():
+        print(element)
+    else:
+        print("1" + element)
